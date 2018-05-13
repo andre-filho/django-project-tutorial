f40459738a1c24db8fff00a5f0908c72b5b5424a
Andre Filho <andre.filho001@outlook.com>
2018-05-13 12:11:13 -0300

    [UPDATE] advancing in the tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-
-# Create your views here.
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    ctx = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', ctx)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    ctx = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', ctx)
 
 def results(request, question_id):
     response = "Results for question %s"
